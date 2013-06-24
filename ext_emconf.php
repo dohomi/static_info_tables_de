0131adcf8b42237add96803ed66424608d3f2dc3
Dominic Garms <djgarms@gmail.com>
2013-06-24 13:23:06 +0800

    [INIT] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+########################################################################
+# Extension Manager/Repository config file for ext: "static_info_tables_de"
+#
+# Auto generated 20-07-2005 12:06
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'Static Info Tables (de)',
+	'description' => 'German (de) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '6.0.4',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Dominic',
+	'author_email' => 'djgarms@gmail.com',
+	'author_company' => 'DMFMedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array (
+		'depends' => array(
+		    'static_info_tables' => '6.0.4-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+?>
